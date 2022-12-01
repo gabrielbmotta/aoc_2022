4f3992ee8e27ee528a0a0ff1b5a07a69db050493
RDoerfel <doerfelruben@aol.com>
2022-12-01 10:05:34 +0100

    get sum of max 3 calories

--- day_01/rd_py/main.py
+++ day_01/rd_py/main.py
@@ -10,12 +10,12 @@ def read_input_lines(file):
         input_data = f.read().splitlines()
     return input_data
 
-def get_max_calories_and_elf(input_data):
-    """ Get max calories and elf from input data
+def get_calories_per_elf(input_data):
+    """ Get calories per elf from input data
     args:
         input_data (list): list of lines
     returns:
-        int, int: max calories and elf"""
+        list: calories per elf"""
     elf = 0
     calories_per_elf = []
     ncalories = 0
@@ -30,14 +30,43 @@ def get_max_calories_and_elf(input_data):
         else:
             ncalories += int(calories)
 
-    # find elf with max calories
+    return calories_per_elf
+
+def sort_calories(calories):
+    """ Sort calories per elf
+    args:
+        calories_per_elf (list): list of calories
+    returns:
+        list: sorted list of calories"""
+    return sorted(calories, reverse=True)
+
+def get_max_calories_and_elf(calories_per_elf):
+    """ Get max calories per elf
+    args:
+        calories_per_elf (list): list of calories per elf
+    returns:
+        int, int: max calories and elf"""
     max_calories = max(calories_per_elf)
     max_elf = calories_per_elf.index(max_calories)
     return max_calories, max_elf
 
+def sum_n_max_calories(sorted_calories, n):
+    """ Sum the n max calories
+    args:
+        sorted_calories (list): sorted list of calories per elf
+        n (int): number of max calories to sum
+    returns:
+        int: number of n max calories"""
+    return sum(sorted_calories[:n])
+
 if __name__ == "__main__":
     file = Path(__file__).parent / "input.txt"
     calories = read_input_lines(file)
+    calories_per_elf = get_calories_per_elf(calories)
+    max_calories, max_elf = get_max_calories_and_elf(calories_per_elf)
+    print(f"Max calories: {max_calories} by elf {max_elf}")
+    sorted_calories_per_elf = sort_calories(calories_per_elf)
+    max3_calories = sum_n_max_calories(sorted_calories_per_elf, 3)
+    print(f"Sum of 3 max calories: {max3_calories}")
 
-    max_calories, max_elf = get_max_calories_and_elf(calories)
-    print(f"Elf {max_elf} has the most calories: {max_calories}")
+
