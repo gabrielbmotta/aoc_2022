43d86a7e67e42c1a0e6a2bfd2a73052f3df6ebed
RDoerfel <doerfelruben@aol.com>
2022-12-03 13:29:42 +0100

    task 1

--- day_03/rd_py/main.py
+++ day_03/rd_py/main.py
@@ -6,9 +6,42 @@ def read_lines(file):
         data = f.read().splitlines()
     return data
 
+def get_compartments(rucksack):
+    """ splits string rucksack into two eqaul sized compartments """
+    n = len(rucksack)
+    return rucksack[:n // 2], rucksack[n // 2:]
+
+def get_priority(letter):
+    """ returns the priority of a letter lowercase a-z ranges from 1-26, A-Z ranges from 27-52 """
+    if letter.islower():
+        return ord(letter) - 96
+    else:
+        return ord(letter) - 64 + 26
+
+def get_duplicates(comp1, comp2):
+    """ returns the duplicate letters in two compartments """
+    duplicates = set()
+    for letter in comp1:
+        if letter in comp2:
+            duplicates.add(letter)
+    return duplicates
+
 if __name__ == '__main__':
-    file = Path(__file__).parent / "test.txt"
+    file = Path(__file__).parent / "input.txt"
     lines = read_lines(file)
-    print(lines)
+    total_priority = 0
+    for rucksack in lines:
+        comp1, comp2 = get_compartments(rucksack)
+        duplicates = get_duplicates(comp1, comp2)
+        for element in duplicates:
+            priotity = get_priority(element)
+            total_priority += priotity
+    print(f"Total priority {total_priority}")
+
+
+# get_priotity
+
+
+
 
 # %%
