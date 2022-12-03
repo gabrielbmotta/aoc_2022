e8b72008a176ebf6fe7d179fe8d52b57e0f1330e
RDoerfel <doerfelruben@aol.com>
2022-12-03 13:42:04 +0100

    task 2

--- day_03/rd_py/main.py
+++ day_03/rd_py/main.py
@@ -26,10 +26,27 @@ def get_duplicates(comp1, comp2):
             duplicates.add(letter)
     return duplicates
 
+def get_groups_of_three(lines):
+    """ returns the groups of three lines """
+    groups = []
+    for i in range(0, len(lines), 3):
+        groups.append(lines[i:i+3])
+    return groups
+
+def get_duplicates_in_groups(group):
+    """ returns the duplicates in a group of three lines """
+    duplicates = set()
+
+    for letter in group[0]:
+        if letter in group[1] and letter in group[2]:
+            duplicates.add(letter)
+    return duplicates
+
 if __name__ == '__main__':
     file = Path(__file__).parent / "input.txt"
     lines = read_lines(file)
     total_priority = 0
+
     for rucksack in lines:
         comp1, comp2 = get_compartments(rucksack)
         duplicates = get_duplicates(comp1, comp2)
@@ -38,9 +55,14 @@ if __name__ == '__main__':
             total_priority += priotity
     print(f"Total priority {total_priority}")
 
-
-# get_priotity
-
+    groups = get_groups_of_three(lines)
+    total_priority = 0
+    for group in groups:
+        duplicates = get_duplicates_in_groups(group)
+        for element in duplicates:
+            priotity = get_priority(element)
+            total_priority += priotity
+    print(f"Total priority {total_priority}")
 
 
 
