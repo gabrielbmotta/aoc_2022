e5811f68acaf170e27c23cf4078b49e2221a0551
RDoerfel <doerfelruben@aol.com>
2022-12-04 10:52:17 +0100

    task 2

--- day_04/rd_py/main.py
+++ day_04/rd_py/main.py
@@ -29,15 +29,38 @@ def fully_contains(a,b):
     else:
         return False
 
+def overlaps(a,b):
+    if a[0] >= b[0] and a[0] <= b[1]:
+        return True
+    elif a[1] >= b[0] and a[1] <= b[1]:
+        return True
+    else:
+        return False
+
+def partially_contains(a,b):
+    if overlaps(a,b):
+        return True
+    elif overlaps(b,a):
+        return True 
+    else:
+        return False
+
+
+
 if __name__ == '__main__':
     file = Path(__file__).parent / "input.txt"
     lines = read_lines(file)
+    fullycontains = 0
     overlapping = 0
     for assignment in lines:
         elf1,elf2 = split_assignment(assignment,',')
         range1 = get_range(elf1)
         range2 = get_range(elf2)
         if fully_contains(range1,range2):
+            fullycontains += 1
+        if partially_contains(range1,range2):
             overlapping += 1
-    print(f"Overlapping: {overlapping}")
+    print(f"Fully overlapping: {fullycontains}")
+    print(f"Partially overlapping: {overlapping}")
+
 
