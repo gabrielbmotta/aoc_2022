c4538ae0f984bbd8cddbc914130bb074217c9314
RDoerfel <doerfelruben@aol.com>
2022-12-06 15:31:03 +0100

    read pile from json

--- day_05/rd_py/main.py
+++ day_05/rd_py/main.py
@@ -1,25 +1,14 @@
 #%%
 from pathlib import Path
 import re
+import json
 
 # I know ... :D
-INSTRUCTIONS = {
-    '1':['Z', 'T', 'F', 'R', 'W', 'J', 'G'],
-    '2':['G', 'W', 'M'],    
-    '3':['J', 'N', 'H', 'G'],
-    '4':['J', 'R', 'C', 'N', 'W'],
-    '5':['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M'],
-    '6':['S', 'R', 'T', 'D', 'V', 'W', 'C'],
-    '7':['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V'],
-    '8':['S', 'J', 'N', 'M', 'G', 'C'],
-    '9':['G', 'P', 'N', 'W', 'C', 'J', 'D','L']
-}
 
-TEST = {
-    '1':['Z', 'N'],
-    '2':['M', 'C', 'D'],
-    '3':['P'],
-}
+def read_pile(file):
+    with open(file, "r") as f:
+        pile = json.load(f)
+    return pile
 
 def read_lines(file):
     with open(file, "r") as f:
@@ -31,21 +20,6 @@ def get_instruction(instruction):
     regex.findall(instruction)
     n_, from_, to_ = regex.findall(instruction)
     return [int(n_), from_, to_]
-
-def get_pile():
-    pile = {
-    '1':['Z', 'T', 'F', 'R', 'W', 'J', 'G'],
-    '2':['G', 'W', 'M'],    
-    '3':['J', 'N', 'H', 'G'],
-    '4':['J', 'R', 'C', 'N', 'W'],
-    '5':['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M'],
-    '6':['S', 'R', 'T', 'D', 'V', 'W', 'C'],
-    '7':['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V'],
-    '8':['S', 'J', 'N', 'M', 'G', 'C'],
-    '9':['G', 'P', 'N', 'W', 'C', 'J', 'D','L']
-}
-
-    return pile
 
 def move9000(pile, instruction):
     n_, from_, to_ = get_instruction(instruction)
@@ -78,17 +52,18 @@ def last_element(crate):
 
 if __name__ == "__main__":
     input_instructions = Path(__file__).parent / "input_instruction.txt"    
+    pile_file = Path(__file__).parent / "input_pile.json" 
+
     instructions = read_lines(input_instructions)
-
+    
     # task 1
-    pile = get_pile()
-
+    pile = read_pile(pile_file)
     for instruction in instructions:
         pile = move9000(pile, instruction)
     print_results(pile)
 
     # task 2
-    pile = get_pile()
+    pile = read_pile(pile_file)
     for instruction in instructions:
         pile = move9001(pile, instruction)
     print_results(pile)
