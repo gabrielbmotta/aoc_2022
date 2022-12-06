eb0eb1cff04314ad14614a466a96bfd997d9ffe2
RDoerfel <doerfelruben@aol.com>
2022-12-06 15:25:01 +0100

    task 2

--- day_05/rd_py/main.py
+++ day_05/rd_py/main.py
@@ -15,6 +15,12 @@ INSTRUCTIONS = {
     '9':['G', 'P', 'N', 'W', 'C', 'J', 'D','L']
 }
 
+TEST = {
+    '1':['Z', 'N'],
+    '2':['M', 'C', 'D'],
+    '3':['P'],
+}
+
 def read_lines(file):
     with open(file, "r") as f:
         lines = f.read().splitlines()
@@ -27,31 +33,63 @@ def get_instruction(instruction):
     return [int(n_), from_, to_]
 
 def get_pile():
-    return INSTRUCTIONS
+    pile = {
+    '1':['Z', 'T', 'F', 'R', 'W', 'J', 'G'],
+    '2':['G', 'W', 'M'],    
+    '3':['J', 'N', 'H', 'G'],
+    '4':['J', 'R', 'C', 'N', 'W'],
+    '5':['W', 'F', 'S', 'B', 'G', 'Q', 'V', 'M'],
+    '6':['S', 'R', 'T', 'D', 'V', 'W', 'C'],
+    '7':['H', 'B', 'N', 'C', 'D', 'Z', 'G', 'V'],
+    '8':['S', 'J', 'N', 'M', 'G', 'C'],
+    '9':['G', 'P', 'N', 'W', 'C', 'J', 'D','L']
+}
 
-def move(pile, instruction):
-    n_, from_, to_ = instruction
+    return pile
+
+def move9000(pile, instruction):
+    n_, from_, to_ = get_instruction(instruction)
     return_pile = pile.copy()
     for i in range(n_):
         return_pile[to_].append(return_pile[from_].pop())
     return return_pile
 
+def move9001(pile, instruction):
+    n_, from_, to_ = get_instruction(instruction)
+    return_pile = pile.copy()
+
+    temp_crates = return_pile[from_][-n_:]
+    return_pile[from_] = return_pile[from_][:-n_]
+
+    return_pile[to_] = return_pile[to_] + temp_crates
+
+    return return_pile
+
+def print_results(pile):
+    top_crates = [last_element(pile[key]) for key in pile.keys()]
+    print(''.join(top_crates))
+
+def last_element(crate):
+    if crate == []:
+        return ' '
+    else:
+        return crate[-1]
+
 
 if __name__ == "__main__":
-    input_instructions = Path(__file__).parent / "input_instruction.txt"
-    input_pile = Path(__file__).parent / "input_pile.txt"
-    
-
+    input_instructions = Path(__file__).parent / "input_instruction.txt"    
     instructions = read_lines(input_instructions)
+
+    # task 1
     pile = get_pile()
 
     for instruction in instructions:
-        instruct = get_instruction(instruction)
-        pile = move(pile, instruct)
+        pile = move9000(pile, instruction)
+    print_results(pile)
 
-    top_crates = [pile[key][-1] for key in pile.keys()]
-    print(pile)
-    print(''.join(top_crates))
-
-
+    # task 2
+    pile = get_pile()
+    for instruction in instructions:
+        pile = move9001(pile, instruction)
+    print_results(pile)
 # %%
